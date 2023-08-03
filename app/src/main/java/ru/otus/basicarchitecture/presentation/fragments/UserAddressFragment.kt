b21bef882a744a8535aea8bd2607e2f53d42bc72
Tonius <toniuswar@gmail.com>
2023-08-04 02:00:40 +0300

    Незначительная доработка

--- app/src/main/java/ru/otus/basicarchitecture/presentation/fragments/UserAddressFragment.kt
+++ app/src/main/java/ru/otus/basicarchitecture/presentation/fragments/UserAddressFragment.kt
@@ -1,30 +1,19 @@
 package ru.otus.basicarchitecture.presentation.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 import ru.otus.basicarchitecture.StateFlags
 import ru.otus.basicarchitecture.app.App
 import ru.otus.basicarchitecture.databinding.FragmentUserAddressBinding
-import ru.otus.basicarchitecture.domain.models.NetworkError
-import ru.otus.basicarchitecture.domain.models.NetworkException
-import ru.otus.basicarchitecture.domain.models.NetworkRequest
-import ru.otus.basicarchitecture.domain.models.NetworkSuccess
 import ru.otus.basicarchitecture.domain.models.Suggestions
 import ru.otus.basicarchitecture.domain.models.UserAddress
 import ru.otus.basicarchitecture.presentation.viewmodels.ActivityVMFactory
@@ -35,7 +24,6 @@ import javax.inject.Inject
 
 
 class UserAddressFragment : Fragment() {
-    private val request by lazy { NetworkRequest() }
     private val binding by lazy { FragmentUserAddressBinding.inflate(layoutInflater) }
 
     @Inject
@@ -54,8 +42,6 @@ class UserAddressFragment : Fragment() {
     ): View {
         return binding.root
     }
-
-    @OptIn(FlowPreview::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (requireContext().applicationContext as App).appComponent.injectUserAddressFragment(this)
@@ -81,56 +67,27 @@ class UserAddressFragment : Fragment() {
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                sendData(
-                    binding.editAddress.textChanges().debounce(500)
+                viewModel.sendData(
+                    binding.editAddress.textChanges()
                 )
 
                 viewModel.state.collect { result ->
                     result?.let {
-
-                        when (result) {
-                            is NetworkSuccess<*> -> {
-                                adapter.clear()
-                                val suggestions = result.networkData as Suggestions
-                                suggestions.suggestions.forEach { address ->
-                                    adapter.add(address.value)
-                                }
-                            }
-
-                            is NetworkError -> {
-                                Toast.makeText(
-                                    requireContext(),
-                                    "${result.errorCode}: ${result.errorMessage}",
-                                    Toast.LENGTH_LONG
-
-                                ).show()
-                            }
-
-                            is NetworkException -> {
-                                Log.e("NetworkException", (result.exception as Throwable).message!!)
-                            }
-
+                        adapter.clear()
+                        val suggestions = result.networkData as Suggestions
+                        suggestions.suggestions.forEach { address ->
+                            adapter.add(address.value)
                         }
                     }
 
                 }
+
+
             }
 
 
         }
 
     }
-
-
-    private fun sendData(flow: Flow<String>) {
-        CoroutineScope(Dispatchers.IO).launch {
-            flow.collect {
-                request.query = it
-                viewModel.sendRequest(request)
-            }
-        }
-    }
-
-
 }
 
--- app/src/main/java/ru/otus/basicarchitecture/presentation/viewmodels/UserAddressFragmentViewModel.kt
+++ app/src/main/java/ru/otus/basicarchitecture/presentation/viewmodels/UserAddressFragmentViewModel.kt
@@ -1,15 +1,22 @@
 package ru.otus.basicarchitecture.presentation.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
+import ru.otus.basicarchitecture.domain.models.NetworkError
+import ru.otus.basicarchitecture.domain.models.NetworkException
 import ru.otus.basicarchitecture.domain.models.NetworkRequest
-import ru.otus.basicarchitecture.domain.models.NetworkResult
+import ru.otus.basicarchitecture.domain.models.NetworkSuccess
 import ru.otus.basicarchitecture.domain.models.User
 import ru.otus.basicarchitecture.domain.models.UserAddress
 import ru.otus.basicarchitecture.domain.models.ViewModelData
@@ -28,7 +35,7 @@ class UserAddressFragmentViewModel(
 
     ): ViewModel() {
 
-    private val _state = MutableStateFlow<NetworkResult<Any>?>(null)
+    private val _state = MutableStateFlow<NetworkSuccess<Any>?>(null)
     val state = _state.asStateFlow()
 
     private val mutableOutputData = MutableLiveData<User>()
@@ -36,15 +43,15 @@ class UserAddressFragmentViewModel(
         get() = mutableOutputData
     val inputData = MutableLiveData<ViewModelData>()
 
+    private val request by lazy { NetworkRequest() }
+
     init {
         loadData()
-
     }
 
     private fun loadData(){
         mutableOutputData.value = getDataUseCase.getUser()
     }
-
 
     fun saveData() {
         inputData.value.apply {
@@ -54,20 +61,32 @@ class UserAddressFragmentViewModel(
         loadData()
     }
 
+    @OptIn(FlowPreview::class)
+    fun sendData(flow: Flow<String>){
+        flow.debounce(500)
+        CoroutineScope(Dispatchers.IO).launch{
+            flow.collect{
+                request.query = it
+                sendNetworkRequestUseCase.sendRequest(request)
+                getResult()
+            }
+        }
 
-    suspend fun sendRequest(request: NetworkRequest){
-        sendNetworkRequestUseCase.sendRequest(request)
-        delay(100)
-
-        getResult()
     }
-
-
 
     private fun getResult() {
         viewModelScope.launch {
             getNetworkResponseUseCase.getResponse().collect{
-                _state.value = it
+                when(it){
+                    is NetworkSuccess -> _state.value = it
+
+                    is NetworkError -> Log.e("NetworkError",
+                        "${it.errorCode}: ${it.errorMessage}")
+
+                    is NetworkException -> Log.e("NetworkException",
+                        (it.exception as Throwable)
+                            .message!!)
+                }
 
             }
         }
